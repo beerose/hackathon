3c9b9e99e156e1d32b79d3e7932d6069ef90d013
beerose <alexsandra.sikora@gmail.com>
2024-04-21 13:59:50 +0200

    Update Button types

--- components/Button.tsx
+++ components/Button.tsx
@@ -1,34 +1,48 @@
 import { classNames } from '@/src/utils'
 import { RightArrow } from './RightArrow'
+import { AnchorHTMLAttributes, ButtonHTMLAttributes } from 'react'
 
 type ButtonProps =
   | {
       title: string
       variant?: 'primary' | 'secondary'
-    }
-  | {
-      title: string
-      variant?: 'primary' | 'secondary'
-      href?: string
-    }
+      back?: boolean
+      arrow?: boolean
+    } & ButtonHTMLAttributes<HTMLButtonElement>
 
-export function Button({ title, variant, ...rest }: ButtonProps) {
+type AnchorProps = {
+  title: string
+  variant?: 'primary' | 'secondary'
+  href?: string
+  back?: boolean
+  arrow?: boolean
+} & AnchorHTMLAttributes<HTMLAnchorElement>
+
+export function Button({ title, variant, ...rest }: ButtonProps | AnchorProps) {
   const Component = 'href' in rest ? 'a' : 'button'
 
   return (
     <Component
       className={classNames(
-        'relative h-16 py-8 px-6 w-full transition-all duration-300 ease-in-out text-bold text-xl',
+        'relative py-2 px-6 w-full transition-all duration-300 ease-in-out text-bold',
         variant === 'primary'
           ? 'bg-primary text-black'
           : 'bg-transparent text-white border-2 border-textPrimary/20 hover:bg-primary hover:border-primary hover:text-black',
         'rounded-[1px] flex justify-between items-center space-x-4 text-left'
       )}
+      {...(rest as any)}
     >
+      {rest.back && (
+        <div className="h-[12px] w-[12px] rotate-180">
+          <RightArrow />
+        </div>
+      )}
       <span>{title}</span>
-      <div className="h-[12px] w-[12px]">
-        <RightArrow />
-      </div>
+      {!rest.back && (
+        <div className="h-[12px] w-[12px]">
+          <RightArrow />
+        </div>
+      )}
     </Component>
   )
 }
