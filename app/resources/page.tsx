a289db4340107c14935d2a21fef40e9db299f56c
beerose <alexsandra.sikora@gmail.com>
2024-04-21 14:00:35 +0200

    Update resources page

--- app/resources/page.tsx
+++ app/resources/page.tsx
@@ -1,86 +1,36 @@
-import { Button } from '@/components/Button'
-import Link from 'next/link'
-
-const resources = [
-  {
-    title: 'EdgeDB + Next.js Guide',
-    href: 'https://docs.edgedb.com/guides/tutorials/nextjs_app_router',
-  },
-  {
-    title: 'TypeScript/JavaScript Client Docs',
-    href: 'https://docs.edgedb.com/libraries/js',
-  },
-  {
-    title: 'EdgeDB Cheatsheet',
-    href: 'https://docs.edgedb.com/database/cheatsheets',
-  },
-  {
-    title: 'EdgeDB Cloud Docs',
-    href: 'https://docs.edgedb.com/cloud',
-  },
-  {
-    title: 'Connecting EdgeDB Cloud with Vercel',
-    href: 'https://docs.edgedb.com/cloud/deploy/vercel',
-  },
-  {
-    title: 'EddgeDB Live Tutorial',
-    href: 'https://docs.edgedb.com/tutorial',
-  },
-]
+import ExampleApps from '@/components/ExampleApps'
+import Resource, { resources } from '@/components/Resource'
 
 export default function Page() {
   return (
-    <div className="mt-20 md:mt-32 flex items-center flex-col">
-      <h3 className="text-primary text-5xl font-bold mb-12 text-center">
-        Resources
-      </h3>
-      <div className="grid grid-cols-1 md:grid-cols-2 gap-4">
-        <div className="space-y-4 flex flex-col">
-          {resources.slice(0, 3).map((resource) => (
-            <Link
-              key={resource.title}
-              href={resource.href}
-            >
-              <Button
-                title={resource.title}
-                variant="secondary"
+    <div className="flex flex-col items-center md:w-10/12 lg:w-8/12 xl:w-6/12 z-10">
+      <div className="mt-20 md:mt-32 flex items-center flex-col">
+        <h3 className="text-primary text-5xl font-bold mb-12 text-center">
+          Resources
+        </h3>
+        <div className="grid grid-cols-1 md:grid-cols-2 gap-4">
+          <div className="space-y-4 flex flex-col">
+            {resources.slice(0, 3).map((resource) => (
+              <Resource
+                resource={resource}
+                key={resource.href}
               />
-            </Link>
-          ))}
-        </div>
-        <div className="space-y-4 flex flex-col">
-          {resources.slice(3, 6).map((resource) => (
-            <Link
-              key={resource.title}
-              href={resource.href}
-            >
-              <Button
-                title={resource.title}
-                variant="secondary"
+            ))}
+          </div>
+          <div className="space-y-4 flex flex-col">
+            {resources.slice(3, 6).map((resource) => (
+              <Resource
+                resource={resource}
+                key={resource.href}
               />
-            </Link>
-          ))}
+            ))}
+          </div>
         </div>
-      </div>
-      <h3 className="mt-20 text-primary text-3xl font-bold mb-12 text-center">
-        Example apps
-      </h3>
-      <div className="grid grid-cols-1 md:grid-cols-2 gap-4 w-full">
-        <div className="space-y-4 flex flex-col">
-          <Link href="https://github.com/edgedb/nextjs-edgedb-auth-starter">
-            <Button
-              title="Template: Next.js + Auth"
-              variant="secondary"
-            />
-          </Link>
-        </div>
-        <div className="space-y-4 flex flex-col">
-          <Link href="https://github.com/beerose/edgedb-ai-booking-assistant">
-            <Button
-              title="Demo: Vercel AI SDK"
-              variant="secondary"
-            />
-          </Link>
+        <h3 className="mt-20 text-primary text-3xl font-bold mb-12 text-center">
+          Example apps
+        </h3>
+        <div className="grid grid-cols-1 md:grid-cols-2 gap-4 w-full">
+          <ExampleApps />
         </div>
       </div>
     </div>
